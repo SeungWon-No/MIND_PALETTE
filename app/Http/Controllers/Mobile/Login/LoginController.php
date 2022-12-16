f2e13d8d903076ca2d133edc77b8864aa40d7aff
= <=>
2022-12-16 19:10:30 +0900

    feat 로그인 페이지 (퍼블)

--- app/Http/Controllers/Mobile/Login/LoginController.php
+++ app/Http/Controllers/Mobile/Login/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Mobile\Login;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
@@ -22,15 +23,23 @@ class LoginController extends Controller
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $email = urldecode($request["email"]) ?? "";
+        $pw = urldecode($request["pw"]) ?? "";
+
+        if ( $email == "" || $pw  == "" ) {
+            return "이메일 또는 패스워드를 입력해주세요";
+        }
+        $member = Member::findUser($email,$pw)->first();
+        if ($member) {
+            if (Hash::check($pw, $member->pw)) {
+                $this->login($request,  Member::findMemberInfo($member->mbSrl));
+                return "success";
+            }
+        }
+        return "아이디/패스워드가 일치하지 않습니다.";
+
     }
 
     /**
@@ -76,5 +85,32 @@ class LoginController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+
+    public function login($request, $member) {
+        $request->session()->pull('snsJoin','');
+        $request->session()->pull('login','');
+
+        Member::updateLoginDate($member->mbSrl);
+
+        if ( $request->autoLogin == "true" ) {
+            // dd($request->autoLogin);
+            Cookie::queue(Cookie::forever('AKTV', $member->mbSrl));
+        }
+
+        $loginData = [
+            "mbSrl" => $member->mbSrl,
+            "rating" => $member->rating,
+            "mbPhone" => $member->phone,
+            "mbEmail" => $member->email,
+            "mbName" => $member->mbName,
+            "nickName" => $member->nickName,
+            "auth" => $member->auth,
+            "photo" => $member->photo,
+            "addr" => $member->cpAddr1." ".$member->cpAddr2,
+        ];
+        $request->session()->push('login', $loginData);
+        return redirect('/')->with('loginSuccess','success');
     }
 }
--- resources/views/mobile/login/login.blade.php
+++ resources/views/mobile/login/login.blade.php
@@ -6,27 +6,37 @@
             <fieldset>
                 <legend>로그인 입력 폼</legend>
                 <div class="login-input">
+                    @csrf
                     <!-- common.js의 inputChange에서 valid-error클래스를 무조건 삭제하게 처리해 놓았습니다.(기획에서 코드 입력시 무조건 에러 표현은 사라지게 처리해달라는 내용이 있었습니다.) -->
                     <div class="input">
                         <div class="icon icon-form-id-yellow"></div>
-                        <input type="text" placeholder="이메일" onFocus="inputChange(this);" onKeyUp="inputChange(this);" onblur="inputBlur(this);" class="valid-error">
+                        <input id="email" type="text" placeholder="이메일"
+                               onFocus="inputChange(this);" onKeyUp="inputChange(this);"
+                               onblur="inputBlur(this);">
                     </div>
                     <div class="input">
                         <div class="icon icon-form-pw-yellow"></div>
-                        <input type="password" placeholder="비밀번호"  onFocus="inputChange(this);"onKeyUp="inputChange(this);" onblur="inputBlur(this);" class="valid-error">
+                        <input id="pw" type="password" placeholder="비밀번호"
+                               onFocus="inputChange(this);"onKeyUp="inputChange(this);"
+                               onblur="inputBlur(this);">
                     </div>
                 </div>
                 <div class="login-check">
-                    <label class="form-checkbox"><input type="checkbox"><span class="form-check-icon"><em>로그인 유지</em></span></label>
+                    <label class="form-checkbox" onclick="checkLoginForm()">
+                        <input id="autoLogin"  name="autoLogin" type="checkbox" />
+                        <span class="form-check-icon"><em>로그인 유지</em></span>
+                    </label>
                     <a href="#">계정 정보를 잊으셧나요?</a>
                 </div>
                 <div style="display:block">
                     <div class="form-input-valid font-color-error">
-                        <div style="display:block">이메일을 다시 확인해주세요.</div>
+                        <div id="error-info">이메일을 다시 확인해주세요.</div>
                         <div style="display:block">비밀번호를 다시 확인해주세요.</div>
                     </div>
                 </div>
-                <div class="login-btns"><a href="#" class="btn btn-orange btn-large-size btn-page-action">로그인</a></div>
+                <div class="login-btns">
+                    <a href="javascript:login()" class="btn btn-orange btn-large-size btn-page-action">로그인</a>
+                </div>
                 <div class="login-join">계정이 없으신가요? <a href="#">회원가입</a></div>
             </fieldset>
         </div>
@@ -35,5 +45,67 @@
         @include('/mobile/common/footer')
     </div>
 </div>
+<script>
+    $(window).keypress(function(e){
+        if(e.keyCode == 13) {
+            login();
+        }
+    });
+
+    function checkLoginForm() {
+        var email = $("#email").val();
+        var pw = $("#pw").val();
+
+        if (email === "") {
+            $("#email").addClass("valid-error");
+        }
+
+        if (pw === "") {
+            $("#pw").addClass("valid-error");
+        }
+    }
+
+    function login() {
+        var email = $("#email").val();
+        var pw = $("#pw").val();
+        var autoLogin = document.getElementById('autoLogin').checked;
+
+        $("#error-info").css("display","none");
+        if ( email == "" ) {
+            $("#email").addClass("valid-error");
+            $("#error-info").text("이메일을 입력해주세요");
+            $("#error-info").css("display","block");
+            $("#email").focus();
+            return false;
+        }
+
+        if ( pw == "" ) {
+            $("#pw").addClass("valid-error");
+            $("#error-info").text("비밀번호를 입력해주세요");
+            $("#error-info").css("display","block");
+            $("#pw").focus();
+            return false;
+        }
+
+        $.ajax({
+            type:'POST',
+            url:'/login',
+            data: {
+                "email" : encodeURI(email),
+                "pw" : encodeURI(pw),
+                "autoLogin" : autoLogin
+            },
+            headers: {'X-CSRF-TOKEN': $('input[name="_token"]').val()},
+            success:function(data){
+                if ( data == "success" ) {
+                    location.href = '/';
+                } else {
+                    $("#error-info").text(data);
+                    $("#error-info").css("display","block");
+                }
+            }
+        });
+    }
+</script>
 @include('/mobile/common/end')
 
