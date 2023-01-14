800e3ea5356dbd87e7d2f078ea5bb0e12cd810c4
김태윤 <android_yn@naver.com>
2023-01-14 23:41:45 +0900

    feat [상담사] 공지사항

--- app/Http/Controllers/Advisor/Notice/AdvisorNoticeController.php
+++ app/Http/Controllers/Advisor/Notice/AdvisorNoticeController.php
@@ -11,11 +11,13 @@ class AdvisorNoticeController extends Controller
 {
     public function index(Request $request)
     {
+        $type = $request->type ?? '';
         $advisorPK = $request->session()->get('advisorLogin')[0]["advisorPK"];
 
         return view("/advisor/notice/notice",[
             "advisorProfile" => Advisor::getAdvisorProfile($advisorPK),
-            "noticeList" => NoticeBoard::findNotice()
+            "noticeList" => NoticeBoard::findNotice($type),
+            "type" =>$type
         ]);
     }
 
@@ -24,9 +26,35 @@ class AdvisorNoticeController extends Controller
 
     }
 
-    public function show(Request $request)
+    public function show(Request $request,$noticePK)
     {
-        return view("/advisor/notice/noticeDetail");
+        $advisorPK = $request->session()->get('advisorLogin')[0]["advisorPK"];
+        $notice = NoticeBoard::find($noticePK);
+        $notice->viewCount = $notice->viewCount+1;
+        $notice->save();
+
+        $pages = NoticeBoard::findPageNavigation($noticePK);
+        $pageNavigation = [];
+        foreach ( $pages as $page) {
+
+            if ($page->noticePK < $noticePK) {
+                $pageNavigation["prePage"] = [
+                    "noticePK" => $page->noticePK,
+                    "title" => $page->title,
+                ];
+            } else {
+                $pageNavigation["nextPage"] = [
+                    "noticePK" => $page->noticePK,
+                    "title" => $page->title,
+                ];
+
+            }
+        }
+        return view("/advisor/notice/noticeDetail",[
+            "advisorProfile" => Advisor::getAdvisorProfile($advisorPK),
+            "notice" => $notice,
+            "pageNavigation" => $pageNavigation
+        ]);
     }
 
 }
--- app/Models/NoticeBoard.php
+++ app/Models/NoticeBoard.php
@@ -3,15 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
 class NoticeBoard extends Model
 {
     protected $table = 'noticeBoard';
     protected $primaryKey = 'noticePK';
     public $timestamps = false;
 
-    public static function findNotice() {
-        return NoticeBoard::where("isDelete","N")
-            ->orderBy("noticePK", "DESC")
+    public static function findNotice($type) {
+        $orderBy = ($type == "old") ? "ASC" : "DESC";
+        $pagination = NoticeBoard::where("isDelete","N")
+            ->orderBy("noticePK", $orderBy)
             ->paginate(10);
+        return json_decode(json_encode($pagination), true);
+    }
+
+    public static function findPageNavigation($noticePK) {
+        return DB::select("select * from noticeBoard
+          where ( noticePK = IFNULL((select min(noticePK) from noticeBoard where noticePK > ".$noticePK." ),0)
+                      or  noticePK = IFNULL((select max(noticePK) from noticeBoard where noticePK < ".$noticePK." ),0)
+          )");
     }
 }
