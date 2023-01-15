edcd1fc43a519493aa93a890a4214da35f9e68c0
noseungwon <NSW@gmail.com>
2023-01-15 07:30:34 +0000

    test

--- app/Http/Controllers/Advisor/Join/AdvisorExamineController.php
+++ app/Http/Controllers/Advisor/Join/AdvisorExamineController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Advisor\Join;
 
 use App\Http\Controllers\Controller;
+use App\Models\Advisor;
 use Cookie;
 use Illuminate\Http\Request;
 
@@ -10,6 +11,10 @@ class AdvisorExamineController extends Controller
 {
     public function __invoke(Request $request)
     {
+        $advisorPK = $request->session()->get('advisorLogin')[0]["advisorPK"]; // 132
+        $getAdvisorInfo = Advisor::getAdvisorProfile($advisorPK);
+        dd($getAdvisorInfo);
+
         return view('/advisor/join/examine');
     }
 }
