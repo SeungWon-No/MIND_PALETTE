f9734bf1f58117c6aebdfabe9811883eb474647a
noseungwon <NSW@gmail.com>
2023-01-15 07:58:59 +0000

    test

--- resources/views/advisor/join/examine.blade.php
+++ resources/views/advisor/join/examine.blade.php
@@ -1,5 +1,5 @@
 @php
-print_r($getAdvisorStatus);
+print_r($getAdvisorStatus['getAdvisorStatus']);
 @endphp
 @include('advisor/common/header')
     <div id="container">
@@ -7,23 +7,20 @@ print_r($getAdvisorStatus);
         <div class="member-inner">
           <!-- 예정 -->
           <div class="join-progress__wrap">
-            @if($getAdvisorInfo['advisorStatus'] == 361) 
+            @if($getAdvisorStatus['getAdvisorStatus'] == 361) 
             <h3 class="member-heading__tit">자격심사 검토 예정</h3>
-            @elseif($getAdvisorInfo['advisorStatus'] == 362)
+            @elseif($getAdvisorStatus['getAdvisorStatus'] == 362)
             <h3 class="member-heading__tit">자격심사 검토 중</h3>
             @else
             <h3 class="member-heading__tit">자격심사 미승인</h3>
             @endif
             <p class="join-progress__text">
-              @if($getAdvisorInfo['advisorStatus'] == 361) 
-              <!-- 예정 -->
-              <strong>{{$getAdvisorInfo['advisorName']}}({{$getAdvisorInfo['email']}})님</strong>자격 심사가 진행 예정입니다.<br>심사 검토 이전에는 정보 수정이 가능합니다.
-              @elseif($getAdvisorInfo['advisorStatus'] == 362)
-              <!-- 진행중 -->
-              <strong>{{$getAdvisorInfo['advisorName']}}({{$getAdvisorInfo['email']}})님</strong>자격 심사가 진행중입니다.<br>심사 진행 중에는 정보 수정이 불가능합니다.
+              @if($getAdvisorStatus['getAdvisorStatus'] == 361) 
+                <strong>{{$getAdvisorInfo['advisorName']}}({{$getAdvisorInfo['email']}})님</strong>자격 심사가 진행 예정입니다.<br>심사 검토 이전에는 정보 수정이 가능합니다.
+              @elseif($getAdvisorStatus['getAdvisorStatus'] == 362)
+                <strong>{{$getAdvisorInfo['advisorName']}}({{$getAdvisorInfo['email']}})님</strong>자격 심사가 진행중입니다.<br>심사 진행 중에는 정보 수정이 불가능합니다.
               @else
-              <!-- 반려 -->
-              <strong>{{$getAdvisorInfo['advisorName']}}({{$getAdvisorInfo['email']}})님</strong>상담사 가입에 실패하였습니다.<br>다음 기회에 시도 부탁드립니다.
+                <strong>{{$getAdvisorInfo['advisorName']}}({{$getAdvisorInfo['email']}})님</strong>상담사 가입에 실패하였습니다.<br>다음 기회에 시도 부탁드립니다.
               @endif
             </p>
             <div class="join-progress mg-bt-40 mg-t-170">
