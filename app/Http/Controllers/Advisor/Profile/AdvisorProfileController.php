f4403d36148500de72e77b6287e0d202e371f987
noseungwon <NSW@gmail.com>
2023-01-19 06:32:21 +0000

    프로필 컨트롤러 try-catch 추가

--- app/Http/Controllers/Advisor/Profile/AdvisorProfileController.php
+++ app/Http/Controllers/Advisor/Profile/AdvisorProfileController.php
@@ -11,6 +11,7 @@ use App\Models\Member;
 use App\Models\Qualification;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\DB;
 
 class AdvisorProfileController extends Controller
 {
@@ -43,109 +44,116 @@ class AdvisorProfileController extends Controller
 
     public function store(Request $request)
     {
-        //dd($request);
-        $advisorPK = $request->session()->get('advisorLogin')[0]["advisorPK"];
+        try {
+            DB::beginTransaction();
+            //dd($request);
+            $advisorPK = $request->session()->get('advisorLogin')[0]["advisorPK"];
 
-        $counselingCareer = $request->counselingCareer ?? '';
-        $centerName = $request->centerName ?? '';
+            $counselingCareer = $request->counselingCareer ?? '';
+            $centerName = $request->centerName ?? '';
 
-        if ($counselingCareer != "" || $centerName != "") {
-            $advisor = Advisor::find($advisorPK);
-            if ($counselingCareer != "") {
-                $advisor->career = $counselingCareer;
-            }
-            if ($centerName != "") {
-                $advisor->centerName = $centerName;
-            }
-            $advisor->save();
-        }
-
-        $advisorBriefIntroduction = $request->briefIntroduction ?? '';
-        $advisorDetailedDescription = $request->detailedDescription ?? '';
-
-        if ($advisorBriefIntroduction != "" || $advisorDetailedDescription != "") {
-            $advisor = Advisor::find($advisorPK);
-            if ($advisorBriefIntroduction != "") {
-                $advisor->briefIntroduction = $advisorBriefIntroduction;
-            }
-            if ($advisorDetailedDescription != "") {
-                $advisor->detailedDescription = $advisorDetailedDescription;
-            }
-            $advisor->save();
-        }
-
-        $nowDateTime = date('Y-m-d H:i:s');
-
-        $educationCount = $request->educationCount ?? 0;
-        for ($index = 1; $index <= $educationCount; $index++) {
-
-            $educationPK = $request['educationPK' . $index] ?? '';
-            if ($educationPK == '') {
-                $educationLevel = new EducationLevel;
-            } else {
-                $educationLevel = EducationLevel::find($educationPK);
+            if ($counselingCareer != "" || $centerName != "") {
+                $advisor = Advisor::find($advisorPK);
+                if ($counselingCareer != "") {
+                    $advisor->career = $counselingCareer;
+                }
+                if ($centerName != "") {
+                    $advisor->centerName = $centerName;
+                }
+                $advisor->save();
             }
 
-            $educationLevel->advisorPK = $advisorPK;
-            $educationLevel->degree = $request['degree' . $index];
-            $educationLevel->school = $request['schoolName' . $index];
-            $educationLevel->department = $request['department' . $index];
-            $educationLevel->major = $request['major' . $index];
-            $educationLevel->graduationStatus = $request['graduation' . $index];
-            $educationLevel->certificateFilePath = $request['education-attachedFilePath' . $index];
-            $educationLevel->fileName = $request['education-attachedFileName' . $index];
-            $educationLevel->updateDate = $nowDateTime;
-            $educationLevel->createDate = $nowDateTime;
-            $educationLevel->save();
-        }
+            $advisorBriefIntroduction = $request->briefIntroduction ?? '';
+            $advisorDetailedDescription = $request->detailedDescription ?? '';
 
-        // 자격사항 INSERT
-        $qualificationCount = $request->qualificationCount ?? 0;
-        for ($index = 1; $index <= $qualificationCount; $index++) {
-
-            $qualificationPK = $request['qualificationPK' . $index] ?? '';
-
-            if ($qualificationPK == '') {
-                $qualification = new Qualification;
-            } else {
-                $qualification = Qualification::find($qualificationPK);
+            if ($advisorBriefIntroduction != "" || $advisorDetailedDescription != "") {
+                $advisor = Advisor::find($advisorPK);
+                if ($advisorBriefIntroduction != "") {
+                    $advisor->briefIntroduction = $advisorBriefIntroduction;
+                }
+                if ($advisorDetailedDescription != "") {
+                    $advisor->detailedDescription = $advisorDetailedDescription;
+                }
+                $advisor->save();
             }
 
-            $qualification->advisorPK = $advisorPK;
-            $qualification->issuingAgency = $request['issuance' . $index];
-            $qualification->certificateName = $request['licenseTitle' . $index];
-            $qualification->certificateFilePath = $request['qualification-attachedFilePath' . $index];
-            $qualification->fileName = $request['qualification-attachedFileName' . $index];
-            $qualification->updateDate = $nowDateTime;
-            $qualification->createDate = $nowDateTime;
-            $qualification->save();
-        }
+            $nowDateTime = date('Y-m-d H:i:s');
+
+            $educationCount = $request->educationCount ?? 0;
+            for ($index = 1; $index <= $educationCount; $index++) {
+
+                $educationPK = $request['educationPK' . $index] ?? '';
+                if ($educationPK == '') {
+                    $educationLevel = new EducationLevel;
+                } else {
+                    $educationLevel = EducationLevel::find($educationPK);
+                }
+
+                $educationLevel->advisorPK = $advisorPK;
+                $educationLevel->degree = $request['degree' . $index];
+                $educationLevel->school = $request['schoolName' . $index];
+                $educationLevel->department = $request['department' . $index];
+                $educationLevel->major = $request['major' . $index];
+                $educationLevel->graduationStatus = $request['graduation' . $index];
+                $educationLevel->certificateFilePath = $request['education-attachedFilePath' . $index];
+                $educationLevel->fileName = $request['education-attachedFileName' . $index];
+                $educationLevel->updateDate = $nowDateTime;
+                $educationLevel->createDate = $nowDateTime;
+                $educationLevel->save();
+            }
+
+            // 자격사항 INSERT
+            $qualificationCount = $request->qualificationCount ?? 0;
+            for ($index = 1; $index <= $qualificationCount; $index++) {
+
+                $qualificationPK = $request['qualificationPK' . $index] ?? '';
+
+                if ($qualificationPK == '') {
+                    $qualification = new Qualification;
+                } else {
+                    $qualification = Qualification::find($qualificationPK);
+                }
+
+                $qualification->advisorPK = $advisorPK;
+                $qualification->issuingAgency = $request['issuance' . $index];
+                $qualification->certificateName = $request['licenseTitle' . $index];
+                $qualification->certificateFilePath = $request['qualification-attachedFilePath' . $index];
+                $qualification->fileName = $request['qualification-attachedFileName' . $index];
+                $qualification->updateDate = $nowDateTime;
+                $qualification->createDate = $nowDateTime;
+                $qualification->save();
+            }
 
             // 경력사항 INSERT
-        $careerCount = $request->careerCount ?? 0;
-        for ($index = 1; $index <= $careerCount; $index++) {
-            $careerPK = $request['careerPK' . $index] ?? '';
+            $careerCount = $request->careerCount ?? 0;
+            for ($index = 1; $index <= $careerCount; $index++) {
+                $careerPK = $request['careerPK' . $index] ?? '';
 
-            if ($careerPK == '') {
-                $career = new Career;
-            } else {
-                $career = Career::find($careerPK);
+                if ($careerPK == '') {
+                    $career = new Career;
+                } else {
+                    $career = Career::find($careerPK);
+                }
+
+                $career->advisorPK = $advisorPK;
+                $career->counselingCareer = $request['counselingCareer'];
+                $career->careerType = $request['careerType' . $index];
+                $career->companyName = $request['companyName' . $index];
+                $career->employmentType = $request['employmentType' . $index];
+                $career->assignedTask = $request['assignedTask' . $index];
+                $career->certificateFilePath = $request['career-attachedFilePath' . $index];
+                $career->fileName = $request['career-attachedFileName' . $index];
+                $career->createDate = $nowDateTime;
+                $career->updateDate = $nowDateTime;
+                $career->save();
+
             }
-
-            $career->advisorPK = $advisorPK;
-            $career->counselingCareer = $request['counselingCareer'];
-            $career->careerType = $request['careerType' . $index];
-            $career->companyName = $request['companyName' . $index];
-            $career->employmentType = $request['employmentType' . $index];
-            $career->assignedTask = $request['assignedTask' . $index];
-            $career->certificateFilePath = $request['career-attachedFilePath' . $index];
-            $career->fileName = $request['career-attachedFileName' . $index];
-            $career->createDate = $nowDateTime;
-            $career->updateDate = $nowDateTime;
-            $career->save();
+            DB::commit();
+            return redirect('/advisor/profile/profileUpdate');
+            
+        }catch(\Exception $e){
 
         }
-        return redirect('/advisor/profile/profileUpdate');
     }
 
 
