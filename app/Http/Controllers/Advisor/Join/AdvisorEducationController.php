9d04a4d9fe22f4765ebd026300345845e812cf69
noseungwon <NSW@gmail.com>
2023-01-19 02:44:45 +0000

    승인요청-로그인 임시 수정

--- app/Http/Controllers/Advisor/Join/AdvisorEducationController.php
+++ app/Http/Controllers/Advisor/Join/AdvisorEducationController.php
@@ -133,10 +133,13 @@ class AdvisorEducationController extends Controller{
                 $career->save();
             }
             DB::commit();
+            
             $getAdvisorStatus = Advisor::getAdvisorStatus($advisorPK);
             
-            if($getAdvisorStatus['advisorStatus'] == 361){ // 승인요청
-                return redirect('/advisor/examine');
+            //if($getAdvisorStatus['advisorStatus'] == 361){ // 승인요청
+                //return redirect('/advisor/examine');
+            if($getAdvisorStatus['advisorStatus'] == 2){ // 승인요청
+                return redirect('/advisor/logout')->with('error', '승인 완료 되었습니다 로그인 화면으로 이동합니다.');
 
             }else{  // 임시저장
                 return redirect('/advisor/consultationInformationEdit');
