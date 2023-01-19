53367dface02c6b1178d3bd5d808f86bd1dc09cd
noseungwon <NSW@gmail.com>
2023-01-19 06:05:01 +0000

    test

--- resources/views/advisor/join/consultationInformation.blade.php
+++ resources/views/advisor/join/consultationInformation.blade.php
@@ -566,7 +566,7 @@
       var extraValue = $('#submitExtraValue').val(extraParam);
       var agreeCheckbox = window.agreeCheckbox();
 
-      if(degree == -1 || schoolName == '' || department == '' || major == '' || graduation == ''){
+      if(degree == -1 || schoolName == '' || department == '' || major == '' || graduation == -1){
         pop.open('noEnterDataPop');
         return false;
       }
