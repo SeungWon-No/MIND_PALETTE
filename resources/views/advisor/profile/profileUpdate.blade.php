67000c17303d00248293e30ecced35c6a273594f
noseungwon <NSW@gmail.com>
2023-01-20 12:23:41 +0000

    test

--- resources/views/advisor/profile/profileUpdate.blade.php
+++ resources/views/advisor/profile/profileUpdate.blade.php
@@ -732,7 +732,7 @@
                     $("#"+type+"-attachedFilePath"+imageIndex).val(data.filePath);
                     $("#"+type+"-attachedFileName"+imageIndex).val(data.fileName);
                     $("#"+type+"-attachedDisplayName"+imageIndex).html(data.fileName);
-                    location.reload();
+
                 } else {
                     console.log(data.message);
                 }
